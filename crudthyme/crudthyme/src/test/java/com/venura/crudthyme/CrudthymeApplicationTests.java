a3e60d349e86cbc6c97e7947beae9c59315f01e0
VenuraRajapaksha-2000 <60282548+VenuraRajapaksha-2000@users.noreply.github.com>
2021-08-05 21:08:14 +0530

    Create the complete file structure

---
+++ crudthyme/crudthyme/src/test/java/com/venura/crudthyme/CrudthymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venura.crudthyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudthymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
